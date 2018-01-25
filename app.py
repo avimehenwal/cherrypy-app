f671d05f0772f63c2480197e4000f682f72fb513
avimehenwal <avi.mehanwal@gmail.com>
2018-01-25 13:59:45 +0530

    ci build process

--- app.py
+++ app.py
@@ -13,13 +13,5 @@ class Root(object):
 
 
 if __name__ == '__main__':
-    # The config.update is optional, but this will prevent scaling issues in a moment
-    cherrypy.config.update({
-        'server.socket_host': '0.0.0.0', # 127.0.0.1 is default
-        'server.socket_port': 8081, # 8080 is default
-        'server.thread_pool': 100, # 10 is default
-        'tools.trailing_slash.on': False # True is default
-    })
-
-    cherrypy.quickstart(Root(), '/', "prod.conf")
-
+    cherrypy.config.update("server.conf")
+    cherrypy.quickstart(Root())
