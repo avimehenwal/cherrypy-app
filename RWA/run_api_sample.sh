da07d300cd565a83b45ba108d71b7bf8eeb1c254
avimehenwal <avi.mehanwal@gmail.com>
2018-01-30 20:59:49 +0530

    seperate REST collectio|item|operation scheme implementation

--- RWA/run_api_sample.sh
+++ RWA/run_api_sample.sh
@@ -1,2 +1,12 @@
-curl -X GET http://0.0.0.0:8080
-curl -X GET http://0.0.0.0:8080/api/company/3
+# Homepage
+curl -X GET http://0.0.0.0:8080;
+
+# CollectionAPI
+curl -X GET http://localhost:8080/collection;
+curl -X POST --header 'Content-length: 0' http://localhost:8080/collection;
+
+# ItemAPI
+curl -X GET http://0.0.0.0:8080/item/3;
+curl -X DELETE http://0.0.0.0:8080/item/3;
+curl -X POST --header 'Content-length: 0' http://localhost:8080/item/3;
+curl -X PUT --header 'Content-length: 0' http://localhost:8080/item/3;
--- RWA/rwa.py
+++ RWA/rwa.py
@@ -6,10 +6,16 @@ DB_STRING = "./resource/http_requests.db"
 TABLE_NAME = "COMPANY"
 
 class Root(object):
-
     @cherrypy.expose
     @cherrypy.tools.json_out()      # tools to send json output
     def index(self):
+        return "Welcome to Homepage"
+
+@cherrypy.expose
+class RestApiCollection(object):
+
+    @cherrypy.tools.json_out()
+    def GET(self):
         result = list()
         cherrypy.log("GET LIST")
         with sqlite3.connect(DB_STRING) as con:
@@ -28,29 +34,32 @@ class Root(object):
         cherrypy.log("GET request ready for dispatch")
         return {"results" : result}
 
+    def POST(self):
+        return "Add new collection"
+
 
 @cherrypy.expose
-class RestAPI(object):
-
-    # def _cp_dispatch(self, vpath):
-    #     if len(vpath) == 2:
-    #         cherrypy.request.params['id'] = vpath.pop()  # /band name/
-    #         cherrypy.request.params['db_name'] = vpath.pop() # /album title/
-    #         return self
-
-    def POST(self):
-        return 'post'
+class RestApiItems(object):
 
     @cherrypy.tools.json_out()
-    def GET(self, table, id):
+    def GET(self, id):
         cherrypy.log("RESTAPI GET ID")
         with sqlite3.connect(DB_STRING) as con:
-            QUERY = "SELECT ID, NAME, AGE, ADDRESS, SALARY FROM %s WHERE ID=%s" % (table, id)
+            QUERY = "SELECT ID, NAME, AGE, ADDRESS, SALARY FROM %s WHERE ID=%s" % (TABLE_NAME, id)
             cherrypy.log(QUERY)
             cursor = con.execute(QUERY)
             record = cursor.fetchone()
             print(type(record))    # tuple
         return {"RECORD" : record}
+
+    def PUT(self, id):
+        return "Modify Item"
+
+    def DELETE(self, id):
+        return "remove Item"
+
+    def POST(self, id):
+        return "New Item"
 
 
 if __name__ == '__main__':
@@ -62,6 +71,7 @@ if __name__ == '__main__':
     }
     cherrypy.config.update("server.conf")
     cherrypy.tree.mount(Root(), '/')
-    cherrypy.tree.mount(RestAPI(), '/api', conf)
+    cherrypy.tree.mount(RestApiCollection(), '/collection', conf)
+    cherrypy.tree.mount(RestApiItems(), '/item', conf)
     cherrypy.engine.start()
     cherrypy.engine.block()
